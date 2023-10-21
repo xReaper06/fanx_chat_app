76040e39044db7369866a840c2bec7838467f370
xReaper06 <helldeadpool456@gmail.com>
2023-10-21 10:18:09 +0800

    new updates

--- server-socket/router/authRoute.js
+++ server-socket/router/authRoute.js
@@ -5,7 +5,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../server/public/images'); // Adjust the destination folder as needed
+      cb(null, '../server-socket/public/images'); // Adjust the destination folder as needed
     },
     filename: function (req, file, cb) {
       cb(null,file.originalname);
--- server-socket/router/userRoute.js
+++ server-socket/router/userRoute.js
@@ -1,31 +1,6 @@
 const express = require('express')
 const router = express.Router();
 const { createChatMessage,fetchChatMessages } = require('../controllers/chatController');
-  const TokenValidity = require('../middleware/tokenValidity')
-
-  router.post('/chat/messages',TokenValidity.verifyToken, async (req, res) => {
-    const { roomId, senderId, message } = req.body; // Assuming you send these parameters in the request body
-  
-    try {
-      const newMessage = await createChatMessage(roomId, senderId, message);
-      res.status(201).json(newMessage);
-    } catch (error) {
-      console.error('Error posting chat message:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-  router.get('/chat/:roomId/messages',TokenValidity.verifyToken, async (req, res) => {
-    const { roomId } = req.params;
-  
-    try {
-      const messages = await fetchChatMessages(roomId);
-      res.status(200).json(messages);
-    } catch (error) {
-      console.error('Error fetching chat messages:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
+  const TokenValidity = require('../middleware/tokenValidity');
   module.exports = router;
   
